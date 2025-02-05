d8cfc0988c39dcc39cc9639f0cdb668f71e37b07
Owner <Owner@DESKTOP-R084BOA>
2025-02-05 15:31:54 -0700

    Add project files.

---
+++ Mission05Assignment/wwwroot/js/calculator.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $('#calculateBtn').click(function () {
+        var hours = parseFloat($('#hoursInput').val());
+        var rate = parseFloat($('#rateInput').val());
+
+        if (isNaN(hours) || hours <= 0) {
+            $('#error').show();
+            $('#totalOutput').val('');
+        } else {
+            $('#error').hide();
+            var total = hours * rate;
+            $('#totalOutput').val(total.toFixed(2));
+        }
+    });
+});
